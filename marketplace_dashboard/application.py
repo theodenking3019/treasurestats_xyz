f8ba0620e0964441dd605d5da9aa2be6f160f52f
theodenking <theodenking3019@gmail.com>
2022-02-10 00:22:50 -0500

    turned off debug mode

--- marketplace_dashboard/application.py
+++ marketplace_dashboard/application.py
@@ -367,5 +367,4 @@ def update_inputs(url_path):
         re.findall("(?<=timeinterval=)[a-z0-9_]+", url_path)[0]
 
 if __name__ == '__main__':
-    app.run_server(debug=True, dev_tools_silence_routes_logging = False, dev_tools_props_check = False)
-    # application.run(debug=False, port=8080)
+    application.run(debug=False, port=8080)
